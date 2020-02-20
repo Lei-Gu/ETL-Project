36cb4f16e4010ced2dc53554177dcf7d70779884
Lei-Gu <leigu46@gmail.com>
2020-02-19 21:36:12 -0600

    updated scripts

--- Chicago Resturant Inspection.sql
+++ Chicago Resturant Inspection.sql
@@ -1,32 +1,70 @@
-CREATE TABLE inspections (
-id INT PRIMARY KEY,
-license_NO Int,
-DBA_name Varchar,
-AKA_name Varchar,
-risk INT,
-inspection_date DATE,
-inspection_type Varchar,
-results Varchar,
-violations Varchar
-);
+DROP TABLE inspections
+DROP TABLE licenses
+DROP TABLE businessinfo
+DROP TABLE owners
+DROP TABLE yelp
+
 select * from inspections;
+Select * from licenses;
+Select * from owners;
+Select * from businessinfo;
+
 
 create table licenses (
-	id int primary key,
-	license_id int,
+	license_id int primary key,
 	account_number int,
+	license_code int,
+	license_description Varchar,
+	license_start_date date,
+	lincese_end_date date,
+	license_status Varchar
+	
+);
+
+create table businessinfo (
+	account_number int primary key,
 	legal_name Varchar,
 	business_name Varchar,
 	address Varchar,
 	city Varchar,
 	state Varchar,
 	zip_code int,
-	license_code int,
-	license_description Varchar,
-	license_start_date date,
-	lincese_end_date date,
-	license_status Varchar,
-	LATITUDE int,
-	LONGITUDE int
+	latitude int,
+	longitude int
 );
-Select * from licenses;
+
+
+CREATE TABLE inspections (
+id INT PRIMARY KEY,
+license_id Int,
+FOREIGN KEY (license_id) REFERENCES licenses(license_id),
+DBA_name Varchar,
+AKA_name Varchar,
+risk TEXT,
+inspection_date DATE,
+inspection_type Varchar,
+results Varchar,
+violations Varchar
+);
+
+
+create table owners (
+	account_number int primary key,
+	FOREIGN KEY (account_number) REFERENCES businessinfo(account_number),
+	legal_name Varchar,
+	owner_first_name Varchar,
+	owner_last_name Varchar,
+	title Varchar);
+	
+
+Create table yelp (
+bus_id Varchar primary key,
+dba_name Varchar,
+license_id Int,
+cust_rating float,
+price_rating float,
+foreign key (license_id) references licenses(license_id)
+);
+
+
+
